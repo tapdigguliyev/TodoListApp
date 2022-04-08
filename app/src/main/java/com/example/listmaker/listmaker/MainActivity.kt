530a0092e212bc8ed2fa861e8abf7c24c15e84a8
Tapdig Guliyev <tapdigguliyev@gmail.com>
2022-04-08 17:30:23 +0400

    Recycler view done with simple todoLists array as data

--- app/src/main/java/com/example/listmaker/listmaker/MainActivity.kt
+++ app/src/main/java/com/example/listmaker/listmaker/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.listmaker.listmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.listmaker.listmaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var todoListRecyclerView: RecyclerView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        todoListRecyclerView = findViewById(R.id.rvTodoList)
+        todoListRecyclerView.layoutManager = LinearLayoutManager(this)
+        todoListRecyclerView.adapter = TodoListAdapter()
     }
 }
---
+++ app/src/main/java/com/example/listmaker/listmaker/TodoListAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.listmaker.listmaker
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoListAdapter() : RecyclerView.Adapter<TodoListAdapter.TodoListViewHolder>() {
+    var todoLists = arrayOf("Android development", "House Work", "Errands", "Shopping")
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.todo_list_view_holder, parent, false)
+        return TodoListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
+        holder.itemNumber.text = (position + 1).toString()
+        holder.itemString.text = todoLists[position]
+    }
+
+    override fun getItemCount() = todoLists.size
+
+    inner class TodoListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemNumber : TextView = itemView.findViewById(R.id.itemNumber)
+        val itemString : TextView = itemView.findViewById(R.id.itemString)
+    }
+}
